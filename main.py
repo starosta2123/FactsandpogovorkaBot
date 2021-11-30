230899f8ced3b0defde2d91a37040f703611fc23
starosta2123 <[admin@mbou69.com]>
2021-11-30 13:43:00 +0400

    Добавил файл с поговорками

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ from telebot import types
 f = open('data/facts.txt', 'r', encoding='UTF-8')
 facts = f.read().split('\n')
 f.close()
+
+f = open('data/thinks.txt', 'r', encoding='UTF-8')
+thinks  = f.read().split('\n')
+f.close()
