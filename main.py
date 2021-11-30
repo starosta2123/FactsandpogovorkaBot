dd58bce83168a59a7493f4c655795dee540e0e21
starosta2123 <[admin@mbou69.com]>
2021-11-30 14:05:07 +0400

    Получаем сообщение от юзера

--- main.py
+++ main.py
@@ -34,3 +34,14 @@ def handle_text(message):
     bot.send_message(message.chat.id, answer)
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    # Если юзер прислал 1, выдаем ему случайный факт
+    if message.text.strip() == 'Факт' :
+            answer = random.choice(facts)
+    # Если юзер прислал 2, выдаем умную мысль
+    elif message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
