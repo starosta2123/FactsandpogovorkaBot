9ccd65ab5d1902450150d5f4ed60b3e513aae3de
starosta2123 <[admin@mbou69.com]>
2021-11-30 14:07:52 +0400

    Добавление коментариев

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import telebot
 import random
 from telebot import types
-
+# Загружаем список интересных фактов
 f = open('data/facts.txt', 'r', encoding='UTF-8')
 facts = f.read().split('\n')
 f.close()
-
+# Загружаем список поговорок
 f = open('data/thinks.txt', 'r', encoding='UTF-8')
 thinks  = f.read().split('\n')
 f.close()
-
+# Создаем бота
 bot = telebot.TeleBot('2115904546:AAEqr_FUahUjhPkyVJaklybJtCVx6xGCZdk')
-
+# Команда start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
         # Добавляем две кнопки
@@ -34,16 +34,3 @@ def handle_text(message):
     bot.send_message(message.chat.id, answer)
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
-
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    # Если юзер прислал 1, выдаем ему случайный факт
-    if message.text.strip() == 'Факт' :
-            answer = random.choice(facts)
-    # Если юзер прислал 2, выдаем умную мысль
-    elif message.text.strip() == 'Поговорка':
-            answer = random.choice(thinks)
-    # Отсылаем юзеру сообщение в его чат
-    bot.send_message(message.chat.id, answer)
-    
-    bot.polling(none_stop=True, interval=0)
