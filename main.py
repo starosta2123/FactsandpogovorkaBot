030a803640b7f2ec569f9cd96b9a5c8e6eefd5cd
starosta2123 <[admin@mbou69.com]>
2021-11-30 14:03:45 +0400

    Добавил Start

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ thinks  = f.read().split('\n')
 f.close()
 
 bot = telebot.TeleBot('2115904546:AAEqr_FUahUjhPkyVJaklybJtCVx6xGCZdk')
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+        # Добавляем две кнопки
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Факт")
+        item2=types.KeyboardButton("Поговорка")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(m.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',  reply_markup=markup)
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    # Если юзер прислал 1, выдаем ему случайный факт
+    if message.text.strip() == 'Факт' :
+            answer = random.choice(facts)
+    # Если юзер прислал 2, выдаем умную мысль
+    elif message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
